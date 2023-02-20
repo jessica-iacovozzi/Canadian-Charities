e3bb15038cafabf7fa5407644a127d30343fa894
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2023-02-19 21:04:32 -0500

    Data filterable and orderable by every attribute but ordering needs refactoring.

--- app/controllers/api/v1/charities_controller.rb
+++ app/controllers/api/v1/charities_controller.rb
@@ -2,16 +2,30 @@ module Api
   module V1
     class CharitiesController < ApplicationController
       def index
-        @charities = Charity.filter(params.slice(:name, :sector, :city, :rating, :slogan))
+        @charities = Charity.filter(params.slice(:name, :sector, :city, :slogan, :website, :rating,
+                                                 :grade, :demonstrated_impact, :cents_to_cause_ratio))
+        grade_order = { '+' => 0, nil => 1, '-' => 2 }
+        sort = params['sort']
 
-        if params['sort'] == '-rating'
-          attr = 'rating'
-          @charities = @charities.sort_by { |h| -h[attr.to_sym] }.reverse.excluding(@charities.where(rating: 'NR'))
-        elsif params['sort']&.include?('-')
-          attr = params['sort'].sub('-', '')
-          @charities = @charities.sort_by { |h| -h[attr.to_sym] }.reverse
-        elsif params['sort']
-          @charities = @charities.sort_by { |h| h[params['sort'].to_sym] }
+        if sort == '-rating'
+          @charities = @charities.sort_by { |c| -c['rating'.to_sym] }.reverse.excluding(@charities.where(rating: 'NR'))
+        elsif sort == 'grade'
+          @charities = @charities.sort_by { |c| [c['grade'.to_sym][0], grade_order[c['grade'.to_sym][1]]] }
+        elsif sort == '-grade'
+          @charities = @charities.sort_by { |c| [c['grade'.to_sym][0], grade_order[c['grade'.to_sym][1]]] }.reverse.excluding(@charities.where(grade: 'NR'))
+        elsif sort == 'cents_to_cause_ratio'
+          @charities = @charities.sort_by { |c| c['cents_to_cause_ratio'.to_sym].scan(/[\d-]+/).map(&:to_i) }
+        elsif sort == '-cents_to_cause_ratio'
+          @charities = @charities.sort_by { |c| c['cents_to_cause_ratio'.to_sym].scan(/[\d-]+/).map(&:to_i) }.reverse
+        elsif sort == 'demonstrated_impact'
+          @charities = @charities.in_order_of(:demonstrated_impact, %w[High Good Average Fair Low])
+        elsif sort == '-demonstrated_impact'
+          @charities = @charities.in_order_of(:demonstrated_impact, %w[High Good Average Fair Low]).reverse
+        elsif sort&.include?('-')
+          attr = sort.sub('-', '')
+          @charities = @charities.sort_by { |c| -c[attr.to_sym] }.reverse
+        elsif sort
+          @charities = @charities.sort_by { |c| c[params['sort'].to_sym] }
         end
 
         page_num = params['page'] ? params['page'].to_i : 1
@@ -24,7 +38,7 @@ module Api
 
       def meta_data(object)
         {
-          per_page: 20,
+          limit: 20,
           current_page: object.current_page,
           total_pages: object.total_pages,
           total_count: object.total_entries
--- app/models/charity.rb
+++ app/models/charity.rb
@@ -2,6 +2,9 @@ class Charity < ApplicationRecord
   include Filterable
 
   scope :filter_by_rating, ->(rating) { where rating: }
+  scope :filter_by_grade, ->(grade) { where grade: }
+  scope :filter_by_demonstrated_impact, ->(demonstrated_impact) { where demonstrated_impact: }
+  scope :filter_by_cents_to_cause_ratio, ->(cents_to_cause_ratio) { where cents_to_cause_ratio: }
   scope :filter_by_name, ->(name) { where('name like ?', "%#{name}%") }
   scope :filter_by_sector, ->(sector) { where('sector like ?', "%#{sector}%") }
   scope :filter_by_city, ->(city) { where('city like ?', "%#{city}%") }
