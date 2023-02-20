cbc8b5c1c5d340ee49341eb187e0f9fcc5980dae
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2023-02-19 22:37:01 -0500

    Added orderable concern. Scopes don't work.

--- app/controllers/api/v1/charities_controller.rb
+++ app/controllers/api/v1/charities_controller.rb
@@ -1,32 +1,36 @@
 module Api
   module V1
     class CharitiesController < ApplicationController
+      include Orderable
+
+      has_scope :order_by_name
       def index
+        @charities = apply_scopes(Charity).order(ordering_params(params))
         @charities = Charity.filter(params.slice(:name, :sector, :city, :slogan, :website, :rating,
                                                  :grade, :demonstrated_impact, :cents_to_cause_ratio))
-        grade_order = { '+' => 0, nil => 1, '-' => 2 }
-        sort = params['sort']
+        # grade_order = { '+' => 0, nil => 1, '-' => 2 }
+        # sort = params['sort']
 
-        if sort == '-rating'
-          @charities = @charities.sort_by { |c| -c['rating'.to_sym] }.reverse.excluding(@charities.where(rating: 'NR'))
-        elsif sort == 'grade'
-          @charities = @charities.sort_by { |c| [c['grade'.to_sym][0], grade_order[c['grade'.to_sym][1]]] }
-        elsif sort == '-grade'
-          @charities = @charities.sort_by { |c| [c['grade'.to_sym][0], grade_order[c['grade'.to_sym][1]]] }.reverse.excluding(@charities.where(grade: 'NR'))
-        elsif sort == 'cents_to_cause_ratio'
-          @charities = @charities.sort_by { |c| c['cents_to_cause_ratio'.to_sym].scan(/[\d-]+/).map(&:to_i) }
-        elsif sort == '-cents_to_cause_ratio'
-          @charities = @charities.sort_by { |c| c['cents_to_cause_ratio'.to_sym].scan(/[\d-]+/).map(&:to_i) }.reverse
-        elsif sort == 'demonstrated_impact'
-          @charities = @charities.in_order_of(:demonstrated_impact, %w[High Good Average Fair Low])
-        elsif sort == '-demonstrated_impact'
-          @charities = @charities.in_order_of(:demonstrated_impact, %w[High Good Average Fair Low]).reverse
-        elsif sort&.include?('-')
-          attr = sort.sub('-', '')
-          @charities = @charities.sort_by { |c| -c[attr.to_sym] }.reverse
-        elsif sort
-          @charities = @charities.sort_by { |c| c[params['sort'].to_sym] }
-        end
+        # if sort == '-rating'
+        #   @charities = @charities.sort_by { |c| -c['rating'.to_sym] }.reverse.excluding(@charities.where(rating: 'NR'))
+        # elsif sort == 'grade'
+        #   @charities = @charities.sort_by { |c| [c['grade'.to_sym][0], grade_order[c['grade'.to_sym][1]]] }
+        # elsif sort == '-grade'
+        #   @charities = @charities.sort_by { |c| [c['grade'.to_sym][0], grade_order[c['grade'.to_sym][1]]] }.reverse.excluding(@charities.where(grade: 'NR'))
+        # elsif sort == 'cents_to_cause_ratio'
+        #   @charities = @charities.sort_by { |c| c['cents_to_cause_ratio'.to_sym].scan(/[\d-]+/).map(&:to_i) }
+        # elsif sort == '-cents_to_cause_ratio'
+        #   @charities = @charities.sort_by { |c| c['cents_to_cause_ratio'.to_sym].scan(/[\d-]+/).map(&:to_i) }.reverse
+        # elsif sort == 'demonstrated_impact'
+        #   @charities = @charities.in_order_of(:demonstrated_impact, %w[High Good Average Fair Low])
+        # elsif sort == '-demonstrated_impact'
+        #   @charities = @charities.in_order_of(:demonstrated_impact, %w[High Good Average Fair Low]).reverse
+        # elsif sort&.include?('-')
+        #   attr = sort.sub('-', '')
+        #   @charities = @charities.sort_by { |c| -c[attr.to_sym] }.reverse
+        # elsif sort
+        #   @charities = @charities.sort_by { |c| c[params['sort'].to_sym] }
+        # end
 
         page_num = params['page'] ? params['page'].to_i : 1
         @charities = @charities.paginate(page: page_num, per_page: 20)
--- app/models/charity.rb
+++ app/models/charity.rb
@@ -1,5 +1,6 @@
 class Charity < ApplicationRecord
   include Filterable
+  include Orderable
 
   scope :filter_by_rating, ->(rating) { where rating: }
   scope :filter_by_grade, ->(grade) { where grade: }
@@ -8,4 +9,6 @@ class Charity < ApplicationRecord
   scope :filter_by_name, ->(name) { where('name like ?', "%#{name}%") }
   scope :filter_by_sector, ->(sector) { where('sector like ?', "%#{sector}%") }
   scope :filter_by_city, ->(city) { where('city like ?', "%#{city}%") }
+
+  scope :order_by_name, -> { sort_by(:name) }
 end
