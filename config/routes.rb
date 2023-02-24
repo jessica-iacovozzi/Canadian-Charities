2df284976a1d22843a18da881770c3be45628caf
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2023-02-23 22:27:32 -0500

    redirects all paths to 'api/v1/charities'

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  root 'api/v1/charities#index'
+  get 'api/v1/charities', to: 'api/v1/charities#index'
 
   namespace :api do
     namespace :v1 do
       resources :charities, only: %i[index show]
     end
   end
+
+  match '*path' => redirect('api/v1/charities'), via: :all
 end
