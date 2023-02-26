811efbc10692cb5d4a25b9375f4ff89af3dafb1d
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2023-02-26 12:35:20 -0500

    Added scroll to top on page change and created banner component.

--- charity-finder/src/Home.js
+++ charity-finder/src/Home.js
@@ -1,19 +1,23 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import ReactPaginate from 'react-paginate';
+import Banner from './components/Banner';
 
 const API_URL = 'https://canadian-charities.fly.dev/api/v1/charities'
 
 function Home() {
-  const [charities, setCharities] = useState([])
+  const [charities, setCharities] = useState([]);
+  const [pageCount, setpageCount] = useState(0);
 
   useEffect(() => {
     const getCharities = async () => {
       const res = await fetch(
-        `${API_URL}?page=1&sort=name`
+        `${API_URL}?sort=name`
       );
       const data = await res.json();
+      const pages = data.meta.total_pages;
       setCharities(data.data.data);
+      setpageCount(pages)
     };
     getCharities();
   }, []);
@@ -30,41 +34,51 @@ function Home() {
     let currentPage = data.selected + 1
     const charitiesFormServer = await fetchCharities(currentPage);
     setCharities(charitiesFormServer)
+    window.scrollTo(0,0);
   };
 
   return (
-    <div className="container">
-      <div className='row m-2'>
-        {charities.map((charity) => {
-          return (
-            <div className='col-sm-6 col-md-4 v my-2'>
-              <div className='card shadow-sm w-100' style={{ minHeight: 225 }}>
-                <div className='card-body'>
-                  <h5 className='card-title text-center h2'>{charity.attributes.name}</h5>
-                  <h6 className='card-subtile text-center text-muted mb-2'>{charity.attributes.slogan}</h6>
-                  <p className='card-text'>Website: {charity.attributes.website}</p>
-                </div>
-              </div>
-            </div>
-          )
-        })}
+    <div>
+      <Banner />
 
-        <ReactPaginate
-          previousLabel={'Previous'}
-          nextLabel={'Next'}
-          pageCount={41}
-          onPageChange={handlePageClick}
-          containerClassName={'pagination justify-content-center'}
-          pageClassName={'page-item'}
-          pageLinkClassName={'page-link'}
-          previousClassName={'page-item'}
-          previousLinkClassName={'page-link'}
-          nextClassName={'page-item'}
-          nextLinkClassName={'page-link'}
-          breakClassName={'page-item'}
-          breakLinkClassName={'page-link'}
-          activeClassName={'active'}
-        />
+      <div className="container">
+        <div className='row m-3'>
+          {charities.map((charity) => {
+            return (
+              <div key={charity.id} className='col-sm-6 v my-3'>
+                <div className='card shadow-sm w-100' style={{ minHeight: 345 }}>
+                  <div className='card-body'>
+                    <a href={`//${charity.attributes.website}`} target="_blank" className='text-decoration-none text-info'>
+                      <h5 className='card-title text-center h3'>{charity.attributes.name}</h5>
+                    </a>
+                      <h6 className='card-subtile text-center text-muted mb-2'>{charity.attributes.slogan}</h6>
+                      <p className='card-text'>Rating: {charity.attributes.rating}</p>
+                      <p className='card-text'>Grade: {charity.attributes.grade}</p>
+                      <p className='card-text'>Impact per dollar: {charity.attributes.demonstrated_impact}</p>
+                      <p className='card-text'>{charity.attributes.cents_to_cause_ratio} of every dollar donated available for programs, after overhead costs of fundraising and admin/management (excluding surplus).</p>
+                    </div>
+                  </div>
+              </div>
+            )
+          })}
+
+          <ReactPaginate
+            previousLabel={'Previous'}
+            nextLabel={'Next'}
+            pageCount={pageCount}
+            onPageChange={handlePageClick}
+            containerClassName={'pagination justify-content-center mt-3'}
+            pageClassName={'page-item'}
+            pageLinkClassName={'page-link'}
+            previousClassName={'page-item'}
+            previousLinkClassName={'page-link'}
+            nextClassName={'page-item'}
+            nextLinkClassName={'page-link'}
+            breakClassName={'page-item'}
+            breakLinkClassName={'page-link'}
+            activeClassName={'active'}
+          />
+        </div>
       </div>
     </div>
   );
---
+++ charity-finder/src/components/Banner.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import '../App.css';
+
+function Banner() {
+  return(
+    <div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light shadow fixed-top">
+        <div class="container">
+          <a class="navbar-brand" href="/">Charity Finder</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ms-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="/">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <header class="masthead">
+        <div class="container h-100">
+          <div class="row h-100 align-items-center">
+            <div class="col-12 text-center">
+              <h1 class="fw-light">Not all charities are built the same</h1>
+              <p class="lead">Find a trustworthy charity in less than a minute!</p>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default Banner;
