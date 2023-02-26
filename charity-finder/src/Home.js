afb52c79a1aba2cb4b422fb6cefd902cc624ef7a
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2023-02-25 19:36:53 -0500

    added redirect file in react and changed api_url to production url

--- charity-finder/src/Home.js
+++ charity-finder/src/Home.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import Charities from './components/Charities';
 import { useEffect, useState } from 'react';
 
-const API_URL = 'http://localhost:3000/api/v1/charities'
+const API_URL = 'https://canadian-charities.fly.dev/api/v1/charities'
 
 function getAPIData() {
   return axios.get(API_URL).then((response) => response.data.data.data)
