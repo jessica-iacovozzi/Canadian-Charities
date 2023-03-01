407eb1e2740cb92e3a562233132f38464d05f6b7
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2023-02-28 22:02:48 -0500

    Finalized filtering by city and sector. Redesigned banner, dropdowns and cards.

--- charity-finder/src/Home.js
+++ charity-finder/src/Home.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import ReactPaginate from 'react-paginate';
 import Banner from './components/Banner';
 import Footer from './components/Footer';
+import { MDBTooltip } from 'mdb-react-ui-kit';
 
 const API_URL = 'https://canadian-charities.fly.dev/api/v1/charities'
 
@@ -14,7 +15,7 @@ function Home() {
   const [sortingMethod, setSortingMethod] = useState();
   const [attributes, setAttributes] = useState([]);
   const [city, setCity] = useState();
-  // const [filteredCharities, setFilteredCharities] = useState(charities);
+  const [sector, setSector] = useState();
 
   useEffect(() => {
     const getAttributes = async () => {
@@ -41,17 +42,16 @@ function Home() {
       const data = await res.json();
       const pages = data.meta.total_pages;
       setCharities(data.data.data);
-      // setFilteredCharities(data.data.data);
       setPageCount(pages);
     };
     getCharities();
   }, []);
 
-  const fetchCharities = async (currentPage, sort='name', city=null) => {
+  const fetchCharities = async (currentPage, sort='name', city='', sector='') => {
     const res = await fetch(
-      `${API_URL}?page=${currentPage + 1}&sort=${sort}&city=${city}`
+      `${API_URL}?page=${currentPage + 1}&sort=${sort}&city=${city}&sector=${sector}`
     );
-    console.log(`${API_URL}?page=${currentPage + 1}&sort=${sort}&city=${city}`)
+    console.log(`${API_URL}?page=${currentPage + 1}&sort=${sort}&city=${city}&sector=${sector}`)
     const data = await res.json();
     const pages = data.meta.total_pages;
     setPageCount(pages);
@@ -60,11 +60,11 @@ function Home() {
 
   const handlePageClick = async (data) => {
     let currentPage = data.selected;
-    const charitiesFormServer = await fetchCharities(currentPage, sortingMethod, city);
+    const charitiesFormServer = await fetchCharities(currentPage, sortingMethod, city, sector);
     setCharities(charitiesFormServer);
     setCurrentPage(currentPage);
     window.scrollTo({
-      top: 400,
+      top: 855,
       left: 0,
       behavior: 'smooth'
     });
@@ -75,105 +75,149 @@ function Home() {
     let currentPage = 0;
     let sortingMethod = data;
     setSortingMethod(sortingMethod);
-    const charitiesFormServer = await fetchCharities(currentPage, sortingMethod, city);
+    const charitiesFormServer = await fetchCharities(currentPage, sortingMethod, city, sector);
     setCharities(charitiesFormServer);
     window.scrollTo({
-      top: 400,
+      top: 660,
       left: 0,
       behavior: 'smooth'
     });
   };
 
-    const handleCityFilter = async (data) => {
-      setCurrentPage(0);
-      let currentPage = 0;
-      let city = data;
-      setCity(city);
-      console.log(city);
-      const charitiesFormServer = await fetchCharities(currentPage, sortingMethod, city);
-      setCharities(charitiesFormServer);
-      window.scrollTo({
-        top: 400,
-        left: 0,
-        behavior: 'smooth'
-      });
+  const handleCityFilter = async (data) => {
+    setCurrentPage(0);
+    let currentPage = 0;
+    let city = data;
+    setCity(city);
+    console.log(city);
+    const charitiesFormServer = await fetchCharities(currentPage, sortingMethod, city, sector);
+    setCharities(charitiesFormServer);
+    window.scrollTo({
+      top: 660,
+      left: 0,
+      behavior: 'smooth'
+    });
+  };
+
+  const handleSectorFilter = async (data) => {
+    setCurrentPage(0);
+    let currentPage = 0;
+    let sector = data;
+    setSector(sector);
+    console.log(sector);
+    const charitiesFormServer = await fetchCharities(currentPage, sortingMethod, city, sector);
+    setCharities(charitiesFormServer);
+    window.scrollTo({
+      top: 660,
+      left: 0,
+      behavior: 'smooth'
+    });
   };
 
   const cities = [...new Set(attributes.map((charity) => charity.attributes.city.split(',')[0].trim()))].sort();
+  const sectors = [...new Set(attributes.map((charity) => charity.attributes.sector.split('-')[0].trim()))].sort();
 
   return (
     <div>
       <Banner />
-      <div className='row justify-content-center mt-4'>
-        <div className='col-2'>
-          <h3 className='text-center order-title'>Order charities by</h3>
-          <select className='form-select mt-4' onChange={(e) => handleCharitySorting(e.target.value)}>
-            <option value="name">Name</option>
-            <option value="city">City</option>
-            <option value="sector">Sector</option>
-            <option value="rating">Rating</option>
-            <option value="grade">Grade</option>
-            <option value="demonstrated_impact">Impact</option>
-            <option value="cents_to_cause_ratio">Cents to cause</option>
-          </select>
+      <div className='red-bg py-3 d-flex justify-content-evenly'>
+        <div className='row justify-content-center my-4'>
+          <div className='col-2'>
+            <h4 className='text-center order-title muli'>Filter by city</h4>
+            <select className='form-select mt-4 mb-2 muli' onChange={(e) => handleCityFilter(e.target.value)}>
+              <option value=''>City</option>
+              {cities.map((city) => {
+                return (
+                  <option value={city}>{city}</option>
+                )
+              })}
+            </select>
+          </div>
+        </div>
+
+        <div className='row justify-content-center my-4'>
+          <div className='col-2'>
+            <h4 className='text-center order-title muli'>Filter by sector</h4>
+            <select className='form-select mt-4 mb-2 muli' onChange={(e) => handleSectorFilter(e.target.value)}>
+              <option value=''>Sector</option>
+              {sectors.map((sector) => {
+                return (
+                  <option value={sector}>{sector}</option>
+                )
+              })}
+            </select>
+          </div>
+        </div>
+
+        <div className='row justify-content-center my-4'>
+          <div className='col-2'>
+            <h4 className='text-center order-title muli'>Order by</h4>
+            <select className='form-select mt-4 mb-2 muli' onChange={(e) => handleCharitySorting(e.target.value)}>
+              <option value="name">Name</option>
+              <option value="city">City</option>
+              <option value="sector">Sector</option>
+              <option value="rating">Rating</option>
+              <option value="grade">Grade</option>
+              <option value="demonstrated_impact">Impact</option>
+              <option value="cents_to_cause_ratio">Cents to cause</option>
+            </select>
+          </div>
         </div>
       </div>
 
-      <div className='row justify-content-center mt-4'>
-        <div className='col-2'>
-          <h3 className='text-center order-title'>Filter charities by</h3>
-          <select className='form-select mt-4' onChange={(e) => handleCityFilter(e.target.value)}>
-            <option id='city' defaultValue='city'>City</option>
-            {cities.map((city) => {
-              return (
-                <option value={city}>{city}</option>
-              )
-            })}
-          </select>
-        </div>
-      </div>
-
-      <div className="container">
-        <div className='row m-3'>
+      <div className="container-xxl">
+        <div className='row m-3 d-flex justify-content-evenly'>
           {charities.map((charity) => {
             return (
-              <div key={charity.id} className='col-sm-6 v my-3'>
-                <div className='card shadow w-100' style={{ minHeight: 400 }}>
-                  <div className='card-body p-4'>
-                    <a href={`//${charity.attributes.website}`} rel="noreferrer" target="_blank" className='text-decoration-none text-info'>
-                      <h5 className='card-title text-center h3'>{charity.attributes.name}</h5>
-                    </a>
-                      <h6 className='card-subtile text-center text-muted mb-2'>{charity.attributes.slogan}</h6>
+              <div key={charity.id} className='col-6 v my-3'>
+                <div className='card shadow w-100 h-100 red-bg mt-5'>
+                  <div className='card-body p-5 muli d-flex flex-column'>
+                    <div>
+                      <a href={`//${charity.attributes.website}`} rel="noreferrer" target="_blank" className='text-decoration-none text-info'>
+                        <h5 className='card-title text-center h2 font-weight-bold text-white'>{charity.attributes.name}</h5>
+                      </a>
+                      <h5 className='card-subtile text-center h5 text-white mb-4'>{charity.attributes.slogan}</h5>
+                    </div>
+                    <div>
                       <p className='card-text'>City: {charity.attributes.city}</p>
                       <p className='card-text'>Sector: {charity.attributes.sector}</p>
-                      <p className='card-text'>Rating: {charity.attributes.rating}</p>
-                      <p className='card-text'>Grade: {charity.attributes.grade}</p>
-                      <p className='card-text'>Impact per dollar: {charity.attributes.demonstrated_impact}</p>
+                      <MDBTooltip tag='p' placement="left" title="Rating is based on financial transparency, need for funding, grade, impact per dollar and cents to cause ratio.">
+                        Rating: {charity.attributes.rating}<br></br>
+                      </MDBTooltip>
+                      <MDBTooltip tag='p' placement="left" title="Grade is based on the charity's public reporting of the work it does and the results it achieves.">
+                        Grade: {charity.attributes.grade}<br></br>
+                      </MDBTooltip>
+                      <MDBTooltip tag='p' placement="left" title="Impact per dollar is calculated from available program information.">
+                        Impact per dollar: {charity.attributes.demonstrated_impact}
+                      </MDBTooltip>
                       <p className='card-text'>{charity.attributes.cents_to_cause_ratio} of every dollar donated available for programs, after overhead costs of fundraising and admin/management (excluding surplus).</p>
                     </div>
                   </div>
+                </div>
               </div>
             )
           })}
-
-          <ReactPaginate
-            previousLabel={'Previous'}
-            nextLabel={'Next'}
-            pageCount={pageCount}
-            onPageChange={handlePageClick}
-            forcePage = {currentPage}
-            containerClassName={'pagination justify-content-center mt-3'}
-            pageClassName={'page-item'}
-            pageLinkClassName={'page-link'}
-            previousClassName={'page-item'}
-            previousLinkClassName={'page-link'}
-            nextClassName={'page-item'}
-            nextLinkClassName={'page-link'}
-            breakClassName={'page-item'}
-            breakLinkClassName={'page-link'}
-            activeClassName={'active'}
-          />
         </div>
+      </div>
+
+      <div className='pagination'>
+        <ReactPaginate
+          previousLabel={'Previous'}
+          nextLabel={'Next'}
+          pageCount={pageCount}
+          onPageChange={handlePageClick}
+          forcePage = {currentPage}
+          containerClassName={'pagination justify-content-center mt-3'}
+          pageClassName={'page-item'}
+          pageLinkClassName={'page-link'}
+          previousClassName={'page-item'}
+          previousLinkClassName={'page-link'}
+          nextClassName={'page-item'}
+          nextLinkClassName={'page-link'}
+          breakClassName={'page-item'}
+          breakLinkClassName={'page-link'}
+          activeClassName={'active'}
+        />
       </div>
       <Footer />
     </div>
--- charity-finder/src/components/Banner.js
+++ charity-finder/src/components/Banner.js
@@ -4,38 +4,42 @@ import '../App.css';
 function Banner() {
   return(
     <div>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light shadow fixed-top">
-        <div class="container">
-          <a class="navbar-brand" href="/">Charity Finder</a>
+      <nav class="navbar navbar-expand-lg red-bg shadow fixed-top">
+        <div class="container py-2">
+          <a class="navbar-brand text-white gothic" href="/">Charity Finder</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ms-auto">
               <li class="nav-item active">
-                <a class="nav-link" href="/">Home</a>
+                <a class="nav-link text-white gothic" href="/">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="/">About</a>
+                <a class="nav-link text-white gothic" href="/">About</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="/">Contact</a>
+                <a class="nav-link text-white gothic" href="/">Contact</a>
               </li>
             </ul>
           </div>
         </div>
       </nav>
 
-      <header class="masthead shadow">
-        <div class="container h-100">
-          <div class="row h-100 align-items-center">
-            <div class="col-12 text-center">
-              <h1 class="fw-light">Not all charities are built the same</h1>
-              <p class="lead">Find a charity you can trust!</p>
+      <div class="container col-xxl-8 py-5">
+        <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+          <div class="col-10 col-sm-8 col-lg-6">
+            <img src={require('../Donate-Charity-PNG-Image.png')} class="d-block mx-lg-auto img-fluid" alt="Charity" width="700" height="400" loading="lazy"/>
+          </div>
+          <div class="col-lg-6">
+            <h1 class="display-5 fw-bold lh-1 mb-3">Not all charities are built the same</h1>
+            <p class="lead">Find one you can trust.</p>
+            <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+              <button onClick={() => {window.scrollTo({ top: 660, left: 0, behavior: 'smooth' })}} type="button" class="btn btn-lg px-4 me-md-2 red-bg">Get started</button>
             </div>
           </div>
         </div>
-      </header>
+      </div>
     </div>
   )
 }
