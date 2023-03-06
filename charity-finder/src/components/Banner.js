67112f80e35a5155dc8a8fe91ac982076a1961c4
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2023-03-05 22:12:26 -0500

    Added favicon and metadata

--- charity-finder/src/components/Banner.js
+++ charity-finder/src/components/Banner.js
@@ -27,7 +27,7 @@ function Banner() {
         </div>
       </nav>
 
-      <div className="container pt-4">
+      <div className="container pt-5">
         <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
           <div id='world' className="col-10 col-sm-8 col-lg-6">
             <img src={require('../Donate-Charity-PNG-Image.png')} className="d-block mx-lg-auto img-fluid" alt="Charity" width="700" height="400" loading="lazy"/>
