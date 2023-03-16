8c5463d6e079f18fa798f81557dd8eed3bc8aa0a
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2023-03-16 19:28:15 -0400

    Animated all pages with css

--- charity-finder/src/components/Banner.js
+++ charity-finder/src/components/Banner.js
@@ -2,7 +2,6 @@ import React from 'react';
 import '../css/Home.css';
 import '../css/Banner.css';
 import { scrollTo } from './Home';
-import { Fade, Rotate } from "react-awesome-reveal";
 
 function Banner() {
   return(
@@ -10,25 +9,17 @@ function Banner() {
       <div className="container">
         <div className="row flex-lg-row-reverse align-items-center pb-5 mb-5">
           <div id='world' className="col-10 col-sm-8 col-lg-6">
-            <Fade triggerOnce delay={800} duration={1500}>
-              <img src={require('../media/heart-hands-2.png')} className="d-block mx-lg-auto img-fluid" alt="Charity" width="400px" height="400px" />
-            </Fade>
+              <img id='home-logo' src={require('../media/heart-hands-2.png')} className="d-block mx-lg-auto img-fluid" alt="Charity" width="400px" height="400px" />
           </div>
           <div id='banner' className="col-12 col-xl-6">
-            <Rotate triggerOnce delay={500}>
-              <h1 className="display-2 lh-1 mt-5">Not all charities are built the same</h1>
-            </Rotate>
+              <h1 id='home-title' className="display-2 lh-1 mt-5">Not all charities are built the same</h1>
             <div className='slo-arr'>
-              <Fade triggerOnce delay={1000}>
                 <p className="slogan">Find one you can trust.</p>
-              </Fade>
-              <Fade triggerOnce delay={1000} duration={1500}>
                 <div onClick={() => {scrollTo("filter-bar")}} className='arrows'>
-                  <span className="m_scroll_arrows unu"></span>
-                  <span className="m_scroll_arrows doi"></span>
-                  <span className="m_scroll_arrows trei"></span>
+                  <span className="m_scroll_arrows one"></span>
+                  <span className="m_scroll_arrows two"></span>
+                  <span className="m_scroll_arrows three"></span>
                 </div>
-              </Fade>
             </div>
           </div>
         </div>
--- charity-finder/src/components/Contact.js
+++ charity-finder/src/components/Contact.js
@@ -7,7 +7,6 @@ import "../css/Contact.css";
 import { BsLinkedin, BsGithub, BsDownload } from "react-icons/bs";
 import { MdOutlineWeb, MdOutlineMail } from "react-icons/md";
 import { BiCoffeeTogo } from "react-icons/bi";
-import { Slide } from "react-awesome-reveal";
 
 function Contact() {
   return(
@@ -16,42 +15,44 @@ function Contact() {
       <div id="page-wrap">
         <Navbar />
         <div className='contact-banner'>
-          <Slide triggerOnce>
-          <h1>Let's get in touch!</h1>
-          </Slide>
+          Let's get in touch
+          <div>Let's get in touch</div>
+          <div>Let's get in touch</div>
+          <div>Let's get in touch</div>
+          <div>Let's get in touch</div>
         </div>
         <div className='row links mx-auto'>
-          <div className="col d-flex flex-wrap flex-column align-items-center align-content-center pt-4">
+          <div id='link-1' className="col d-flex flex-wrap flex-column align-items-center align-content-center pt-4">
             <h4>LinkedIn</h4>
             <a href='https://www.linkedin.com/in/jessica-iacovozzi/' rel="noreferrer" target='_blank' >
                 <BsLinkedin className='icon' />
             </a>
           </div>
-          <div className="col d-flex flex-wrap flex-column align-items-center align-content-center pt-4">
+          <div id='link-2' className="col d-flex flex-wrap flex-column align-items-center align-content-center pt-4">
             <h4>GitHub</h4>
             <a href='https://github.com/jessica-iacovozzi/' rel="noreferrer" target='_blank'>
                 <BsGithub className='icon' />
             </a>
           </div>
-          <div className="col d-flex flex-wrap flex-column align-items-center align-content-center pt-4">
+          <div id='link-3' className="col d-flex flex-wrap flex-column align-items-center align-content-center pt-4">
             <h4>Projects</h4>
             <a href='https://troopl.com/jessicaiacovozzi/' rel="noreferrer" target='_blank'>
                 <MdOutlineWeb className='icon' />
             </a>
           </div>
-          <div className="col d-flex flex-wrap flex-column align-items-center align-content-center pt-4">
+          <div id='link-4' className="col d-flex flex-wrap flex-column align-items-center align-content-center pt-4">
             <h4>Resume</h4>
             <a href='Resume.pdf' download>
                 <BsDownload className='icon' />
             </a>
           </div>
-          <div className="col d-flex flex-wrap flex-column align-items-center align-content-center pt-4">
+          <div id='link-5' className="col d-flex flex-wrap flex-column align-items-center align-content-center pt-4">
             <h4>Email Me</h4>
             <a href="mailto:iacovozzi.jessica@gmail.com">
                 <MdOutlineMail className='icon' />
             </a>
           </div>
-          <div className="col d-flex flex-wrap flex-column align-items-center align-content-center pt-4">
+          <div id='link-6' className="col d-flex flex-wrap flex-column align-items-center align-content-center pt-4">
             <h4>Coffee?</h4>
             <a href='https://www.buymeacoffee.com/jiacovozzi' rel="noreferrer" target='_blank'>
                 <BiCoffeeTogo className='icon' />
